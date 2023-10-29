b782e3836288daefe3ac3b47a788254cbbdbd9af
svetlana.ezheleva <sourire06@mail.ru>
2023-10-29 23:13:34 +0300

    fix pagination

--- src/main/java/by/teachmeskills/springbootproject/controllers/OrderController.java
+++ src/main/java/by/teachmeskills/springbootproject/controllers/OrderController.java
@@ -5,47 +5,28 @@ import by.teachmeskills.springbootproject.entities.User;
 import by.teachmeskills.springbootproject.exceptions.AuthorizationException;
 import by.teachmeskills.springbootproject.exceptions.CartIsEmptyException;
 import by.teachmeskills.springbootproject.services.OrderService;
-import by.teachmeskills.springbootproject.services.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import static by.teachmeskills.springbootproject.ShopConstants.CART;
-import static by.teachmeskills.springbootproject.ShopConstants.PRODUCT_ID;
-import static by.teachmeskills.springbootproject.PagesPathEnum.CART_PAGE;
 import static by.teachmeskills.springbootproject.ShopConstants.USER;
 
 @RestController
 @RequestMapping("/cart")
 @SessionAttributes({CART})
 @AllArgsConstructor
-public class CartController {
+public class OrderController {
+    private final OrderService orderService;
 
-    private final ProductService productService;
-
-    @GetMapping("/open")
-    public ModelAndView openCartPage() {
-        return new ModelAndView(CART_PAGE.getPath());
-    }
-
-    @GetMapping("/add")
-    public ModelAndView addProductToCart(@RequestParam(PRODUCT_ID) int id, @ModelAttribute(CART) Cart cart) {
-        return cart.addProductToCart(productService.findById(id), cart);
-    }
-
-    @GetMapping("/delete")
-    public ModelAndView removeProductFromCart(@RequestParam(PRODUCT_ID) int id, @ModelAttribute(CART) Cart cart) {
-        return cart.removeProductFromCart(id, cart);
-    }
-
-    @ModelAttribute("cart")
-    public Cart shoppingCart() {
-        return new Cart();
+    @GetMapping("/order")
+    public ModelAndView makeOrder(@SessionAttribute(name = USER, required = false) User user,
+                                  @ModelAttribute(CART) Cart cart) throws CartIsEmptyException, AuthorizationException {
+        return orderService.create(user, cart);
     }
 }
